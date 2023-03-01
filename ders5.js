0654736753bb4f8ece200858ef1e332ff4f1a672
mhhtmva2002 <ilahamaharramovaa@gmail.com>
2023-03-02 03:04:27 +0400

    first commit

---
+++ ders5.js
@@ -0,0 +1,11 @@
+let count = 0;
+  let sum = 0;
+ let arr = [ 1, 2, 3 , 4, 5, 6, 7 ];
+ for (let i =0; i < arr.length; i++)
+ {
+   count++;
+  sum = sum + arr[i];
+ }
+ 
+   console.log(sum / count);
+        
---
+++ derss5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace derss5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int sum = 0;
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+            for (int i =0; i < arr.Length; i++)
+            {
+                
+
+
+                    count++;
+                    sum = sum + arr[i];
+                
+            }
+            Console.WriteLine(sum / count);
+        }
+        
+    }
+}
